6ea2a2a5c58db0901376778d84f0c68343c55e16
Lukasz Kosinski <lukasz.kosinski@phoenix-rtos.com>
2020-02-24 14:33:17 +0100

    ia32: Added saving kernel startup arguments in syspage

--- ia32/config.h
+++ ia32/config.h
@@ -5,9 +5,9 @@
  *
  * Configuration
  *
- * Copyright 2012 Phoenix Systems
+ * Copyright 2012, 2020 Phoenix Systems
  * Copyright 2001, 2005 Pawel Pisarczyk
- * Author: Pawel Pisarczyk
+ * Author: Pawel Pisarczyk, Lukasz Kosinski
  *
  * This file is part of Phoenix-RTOS.
  *
@@ -18,28 +18,30 @@
 #define _CONFIG_H_
 
 
-#define VERSION "1.3"
+#define VERSION      "1.3"
 
 
 /* User interface */
-#define WELCOME "-\\- Phoenix-RTOS loader, version: " VERSION
-#define PROMPT  "(plo)% "
+#define WELCOME      "-\\- Phoenix-RTOS loader, version: " VERSION
+#define PROMPT       "(plo)% "
 
-#define LINESZ  80
-#define HISTSZ  8
+#define LINESZ       80
+#define HISTSZ       8
 
 
 /* Console character attributes */
-#define ATTR_DEBUG    2
-#define ATTR_USER     7
-#define ATTR_INIT     13
-#define ATTR_LOADER   15
-#define ATTR_ERROR    4
+#define ATTR_DEBUG   2
+#define ATTR_USER    7
+#define ATTR_INIT    13
+#define ATTR_LOADER  15
+#define ATTR_ERROR   4
 
 
 /* Kernel file */
-#define KERNEL_PATH   "phoenix"
+#define KERNEL_PATH  "phoenix"
 
+/* Kernel args */
+#define KERNEL_ARGS  "Xpinit"
 
 /* Kernel adresses and sizes */
 #define KERNEL_BASE  0xc0000000
