3270900e19cfd0c34ff8d595e4ab420e1fbb711c
kemonats <kemonast@yandex.com>
2020-09-25 20:38:56 +0200

    phfs.c: fixed array index check

--- armv7m7-imxrt106x/phfs.c
+++ armv7m7-imxrt106x/phfs.c
@@ -25,70 +25,69 @@
 #define TEMP 2
 
 struct {
-    s32 (*open)(u16, char *, u32);
-    s32 (*read)(u16, s32, u32 *, u8 *, u32);
-    s32 (*write)(u16, s32, u32 *, u8 *, u32, u8);
-    s32 (*close)(u16, s32);
-    unsigned int dn;
+	s32 (*open)(u16, char *, u32);
+	s32 (*read)(u16, s32, u32 *, u8 *, u32);
+	s32 (*write)(u16, s32, u32 *, u8 *, u32, u8);
+	s32 (*close)(u16, s32);
+	unsigned int dn;
 } phfs_handlers[PDN_NB];
 
 
 
 s32 phfs_open(u16 pdn, char *name, u32 flags)
 {
-    if (pdn > PDN_NB)
-        return ERR_ARG;
+	if (pdn >= PDN_NB)
+		return ERR_ARG;
 
-    return phfs_handlers[pdn].open(phfs_handlers[pdn].dn, name, flags);
+	return phfs_handlers[pdn].open(phfs_handlers[pdn].dn, name, flags);
 }
 
 
 s32 phfs_read(u16 pdn, s32 handle, u32 *pos, u8 *buff, u32 len)
 {
-    if (pdn > PDN_NB)
-        return ERR_ARG;
+	if (pdn >= PDN_NB)
+		return ERR_ARG;
 
-    return phfs_handlers[pdn].read(phfs_handlers[pdn].dn, handle, pos, buff, len);
+	return phfs_handlers[pdn].read(phfs_handlers[pdn].dn, handle, pos, buff, len);
 }
 
 
 s32 phfs_write(u16 pdn, s32 handle, u32 *pos, u8 *buff, u32 len, u8 sync)
 {
-    if (pdn > PDN_NB)
-        return ERR_ARG;
+	if (pdn >= PDN_NB)
+		return ERR_ARG;
 
-    return phfs_handlers[pdn].write(phfs_handlers[pdn].dn, handle, pos, buff, len, sync);
+	return phfs_handlers[pdn].write(phfs_handlers[pdn].dn, handle, pos, buff, len, sync);
 }
 
 
 s32 phfs_close(u16 pdn, s32 handle)
 {
-    if (pdn > PDN_NB)
-        return ERR_ARG;
+	if (pdn >= PDN_NB)
+		return ERR_ARG;
 
-    return phfs_handlers[pdn].close(phfs_handlers[pdn].dn, handle);
+	return phfs_handlers[pdn].close(phfs_handlers[pdn].dn, handle);
 }
 
 
 void phfs_init(void)
 {
-    int i;
+	int i;
 
-    /* Handlers for flash memories */
-    for (i = 0; i < 2; ++i) {
-        phfs_handlers[PDN_FLASH0 + i].open = flash_open;
-        phfs_handlers[PDN_FLASH0 + i].read = flash_read;
-        phfs_handlers[PDN_FLASH0 + i].write = flash_write;
-        phfs_handlers[PDN_FLASH0 + i].close = flash_close;
-        phfs_handlers[PDN_FLASH0 + i].dn = i;
-    }
+	/* Handlers for flash memories */
+	for (i = 0; i < 2; ++i) {
+		phfs_handlers[PDN_FLASH0 + i].open = flash_open;
+		phfs_handlers[PDN_FLASH0 + i].read = flash_read;
+		phfs_handlers[PDN_FLASH0 + i].write = flash_write;
+		phfs_handlers[PDN_FLASH0 + i].close = flash_close;
+		phfs_handlers[PDN_FLASH0 + i].dn = i;
+	}
 
-    phfs_handlers[PDN_COM1].open = phoenixd_open;
-    phfs_handlers[PDN_COM1].read = phoenixd_read;
-    phfs_handlers[PDN_COM1].write = phoenixd_write;
-    phfs_handlers[PDN_COM1].close = phoenixd_close;
-    phfs_handlers[PDN_COM1].dn = UART_LOADER_ID;
+	phfs_handlers[PDN_COM1].open = phoenixd_open;
+	phfs_handlers[PDN_COM1].read = phoenixd_read;
+	phfs_handlers[PDN_COM1].write = phoenixd_write;
+	phfs_handlers[PDN_COM1].close = phoenixd_close;
+	phfs_handlers[PDN_COM1].dn = UART_LOADER_ID;
 
-
-    return;
+	return;
 }
