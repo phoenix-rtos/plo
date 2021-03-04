ffa21755a9a99a24b4da109b458760c8dfae5b13
HBuczynski <hubert.buczynski@phoenix-rtos.com>
2021-03-04 12:13:46 +0100

    zynq7000: add platform commands
    JIRA:PD-13

---
+++ armv7a9-zynq7000/cmd-board.h
@@ -0,0 +1,24 @@
+/*
+ * Phoenix-RTOS
+ *
+ * plo - operating system loader
+ *
+ * Specific platform commands
+ *
+ * Copyright 2021 Phoenix Systems
+ * Author: Hubert Buczynski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _BOARD_CMD_H_
+#define _BOARD_CMD_H_
+
+
+/* Function handles command to initialize PL with bitstream */
+extern void cmd_loadPL(char *args);
+
+
+#endif
