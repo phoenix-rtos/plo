629fff6c78acb27c74f606ff6368c6b9049d9dd0
Gerard P.Swiderski <gerard.swiderski@phoenix-rtos.com>
2021-05-25 23:47:50 +0200

    console: fix no cursor after system start
    Fix hidden cursor after system start and color settings.
    
    JIRA: RTOS-25

--- lib/console.h
+++ lib/console.h
@@ -20,12 +20,15 @@
 #define CONSOLE_CURSOR_SHOW "\033[?25h"
 
 #define CONSOLE_CLEAR   "\033[2J\033[H"
-#define CONSOLE_BOLD    "\033[0m\033[1m"
-#define CONSOLE_RED     "\033[0m\033[31m"
-#define CONSOLE_GREEN   "\033[0m\033[32m"
-#define CONSOLE_MAGENTA "\033[0m\033[35m"
-#define CONSOLE_CYAN    "\033[0m\033[36m"
-#define CONSOLE_NORMAL  "\033[0m\033[37m"
+#define CONSOLE_NORMAL  "\033[0m"
+#define CONSOLE_BOLD    "\033[1m"
+#define CONSOLE_RED     "\033[31m"
+#define CONSOLE_GREEN   "\033[32m"
+#define CONSOLE_YELLOW  "\033[33m"
+#define CONSOLE_BLUE    "\033[34m"
+#define CONSOLE_MAGENTA "\033[35m"
+#define CONSOLE_CYAN    "\033[36m"
+#define CONSOLE_WHITE   "\033[37m"
 
 
 /* Function sets device's major and minor number on which console should has output */
