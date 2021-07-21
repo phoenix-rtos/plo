ad63d5643569375d7ebe7cf80d583fda2bb90547
HBuczynski <hubert.buczynski@phoenix-rtos.com>
2021-07-21 09:51:20 +0200

    hal/imxrt117x: adapt hal to the new syspage structure
    - errno is removed
    
    JIRA: PD-129

--- hal/armv7m/imxrt/117x/config.h
+++ hal/armv7m/imxrt/117x/config.h
@@ -17,19 +17,23 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
+#ifndef __ASSEMBLY__
+
 #include "imxrt.h"
 #include "peripherals.h"
 #include "../types.h"
 #include "../../mpu.h"
 #include "../../string.h"
 
+#endif
+
 
 #define PATH_KERNEL "phoenix-armv7m7-imxrt117x.elf"
 
-/* Addresses descriptions */
-#define ADDR_SYSPAGE 0x202c0000
+#define SIZE_PAGE    0x200
+#define SIZE_STACK   (8 * SIZE_PAGE)
+#define SIZE_SYSPAGE (8 * SIZE_PAGE)
 
-#define SIZE_STACK 5 * 1024
-#define SIZE_PAGE  0x200
+#define ADDR_STACK (_end + SIZE_SYSPAGE + SIZE_STACK)
 
 #endif
