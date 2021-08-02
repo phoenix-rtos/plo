7dcf2fcffd7893dbd1679f0890cf3eb260f81b03
HBuczynski <hubert.buczynski@phoenix-rtos.com>
2021-08-02 15:50:31 +0200

    hal/imx6ull: add _init, interrupts and exception handlers
     - common code shared by imx6ull and zynq7000 is placed in /hal/armv7a/cpu.*
    
    JIRA: DTR-115

---
+++ hal/armv7a/cpu.c
@@ -0,0 +1,41 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Operating system loader
+ *
+ * ARMv7 Cortex - A
+ *
+ * Copyright 2021 Phoenix Systems
+ * Author: Hubert Buczynski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#include <hal/hal.h>
+
+
+void hal_interruptsDisable(void)
+{
+	__asm__ volatile("cpsid if");
+}
+
+
+void hal_interruptsEnable(void)
+{
+	__asm__ volatile("cpsie if");
+}
+
+
+void hal_cpuInvCache(unsigned int type, addr_t addr, size_t sz)
+{
+	switch (type) {
+		case hal_cpuDCache:
+			/* TODO */
+		case hal_cpuICache:
+			/* TODO */
+		default:
+			break;
+	}
+}
