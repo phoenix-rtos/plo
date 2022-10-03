68b5b3f74aafa805b78d4e92f01a3f32a28d6900
Gerard P.Swiderski <gerard.swiderski@phoenix-rtos.com>
2022-10-03 13:41:45 +0200

    lib/prompt: Add lib_promptConfirm()
    Implements a function that prompts the user to agree to perform an
    operation (action which may be risky like deletion, any action which
    lead to "think before act", do you agree?), or just displays a pause
    type message (press any key to continue, but with custom message).
    
    JIRA: RTOS-261

---
+++ lib/prompt.c
@@ -0,0 +1,61 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Operating system loader
+ *
+ * Interaction with user (prompt, ask, display message)
+ *
+ * Copyright 2022 Phoenix Systems
+ * Author: Gerard Swiderski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#include "lib.h"
+
+
+int lib_promptConfirm(const char *message, const char *answer)
+{
+	char c;
+	unsigned int pos = 0;
+
+	/*
+	 * Use "%s" format in `message` to provide a hint of what to type
+	 * from optional `answer string.
+	 */
+	lib_printf(message, answer);
+
+	for (;;) {
+		lib_consoleGetc(&c, -1);
+
+		if (c == 3) {
+			/* ^C -- abort */
+			return -EINTR;
+		}
+
+		if (answer != NULL && *answer != '\0') {
+			if (answer[pos] == c) {
+				lib_consolePutc(c);
+				pos++;
+
+				if (answer[pos] == '\0') {
+					/* User confirmed by entering answer */
+					return 1;
+				}
+
+				continue;
+			}
+
+			while (pos-- > 0) {
+				lib_consolePuts("\b \b");
+			}
+		}
+
+		break;
+	}
+
+	/* Any other input, or not confirmed answer */
+	return 0;
+}
