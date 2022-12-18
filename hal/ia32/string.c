d51c109d92a6fc2ee78f1a845fe5d9067c0eb1a2
Gerard P.Swiderski <gerard.swiderski@phoenix-rtos.com>
2022-12-19 00:27:44 +0100

    string: Add hal_strchr
    JIRA: RTOS-315

--- hal/ia32/string.c
+++ hal/ia32/string.c
@@ -151,6 +151,18 @@ char *hal_strncpy(char *dest, const char *src, size_t n)
 }
 
 
+char *hal_strchr(const char *s, int c)
+{
+	do {
+		if (*s == c) {
+			return (char *)s;
+		}
+	} while (*(s++));
+
+	return NULL;
+}
+
+
 int hal_i2s(char *prefix, char *s, unsigned int i, unsigned char b, char zero)
 {
 	static const char digits[] = "0123456789abcdef";
