2d30e6782386fd18aaa6172adeda4ad835ad93d7
Hubert Badocha <hubert.badocha@phoenix-rtos.com>
2023-03-02 18:56:03 +0100

    flash-imx6ull: Add optional logic to perform big buffer read.
    JIRA: RTOS-382

--- cmds/test-dev.c
+++ cmds/test-dev.c
@@ -21,38 +21,44 @@
 #include "cmd.h"
 
 
-#define TEST_DEV_TIMEOUT_MS 500
+#define TEST_DEV_TIMEOUT_MS 500u
 
-#define ERASE_BEFORE 1
-#define ERASE_AFTER  2
+#define ERASE_BEFORE 1u
+#define ERASE_AFTER  2u
 
 #define BUF_SIZE 257 /* Write and read size specially not aligned to any power of 2 to possibly test more edge cases. */
+
+#define CMD_TESTDEV_READ_BIG_BUF_SIZE 0 /* Used to perform a check with a read using big buffer. */
 
 
 static void cmd_testDevInfo(void)
 {
-	lib_consolePuts("performs simple dev read/write test, usage:test-dev [-e erase before] [-E erase after] <dev> <addr> <size>");
+	lib_consolePuts("performs simple dev read/write test, usage:test-dev [-e erase before] [-E erase after] -d <dev> [-s <addr> start(default 0)] -l length");
 }
 
 
 static void cmd_testDevUsage(void)
 {
-	lib_consolePuts("Usage: test-dev [-e] [-E] <major>.<minor> <addr> <size>\n"
-					"  -e       Erase before\n"
-					"  -E 		Erase after\n");
+	/* clang-format off */
+	lib_consolePuts("Usage: test-dev [-e] [-E] -d <major>.<minor> -a <addr> [-l <length>]\n"
+	"  -e       Erase before\n"
+	"  -E       Erase after\n"
+	"  -s       Start address, default: 0\n"
+	"  -l       Length\n"
+	);
+	/* clang-format on */
 }
 
 
-static int cmd_testDevWrite(int major, int minor, addr_t addr, size_t length)
+static int cmd_testDevWrite(int major, int minor, addr_t addr, size_t length, u8 *buf, size_t buflen)
 {
 	ssize_t res;
 	size_t rsz, i, chunk;
-	u8 buf[BUF_SIZE];
 
 	for (rsz = 0; rsz < length; rsz += res) {
-		chunk = min((length - rsz), sizeof(buf));
+		chunk = min((length - rsz), buflen);
 		for (i = 0; i < chunk; i++) {
-			buf[i] = ((rsz + i) & 0xff);
+			buf[i] = ((rsz + i) & 0xffu);
 		}
 		res = devs_write(major, minor, addr + rsz, buf, chunk);
 		if (res < 0) {
@@ -64,17 +70,16 @@ static int cmd_testDevWrite(int major, int minor, addr_t addr, size_t length)
 }
 
 
-static int cmd_testDevRead(int major, int minor, addr_t addr, size_t length)
+static int cmd_testDevRead(int major, int minor, addr_t addr, size_t length, u8 *buf, size_t buflen)
 {
 	ssize_t res;
 	size_t rsz, chunk;
-	u8 buf[BUF_SIZE];
 
 	lib_printf("\nRead device %d.%d  from 0x%x to 0x%x (%zu bytes):\n", major, minor, (u32)addr, (u32)addr + length, length);
 	lib_consolePutHLine();
 
 	for (rsz = 0; rsz < length; rsz += res) {
-		chunk = min((length - rsz), sizeof(buf));
+		chunk = min((length - rsz), buflen);
 		res = devs_read(major, minor, addr + rsz, buf, chunk, TEST_DEV_TIMEOUT_MS);
 		if (res < 0) {
 			log_error("\nCan't read data %zd\n", res);
@@ -100,14 +105,13 @@ static int cmd_testDevErase(int major, int minor, addr_t addr, size_t length)
 }
 
 
-static int cmd_testDevCheck(int major, int minor, addr_t addr, size_t length)
+static int cmd_testDevCheck(int major, int minor, addr_t addr, size_t length, u8 *buf, size_t buflen)
 {
 	ssize_t res;
 	size_t rsz, i, chunk;
-	u8 buf[BUF_SIZE];
 
 	for (rsz = 0; rsz < length; rsz += res) {
-		chunk = min((length - rsz), sizeof(buf));
+		chunk = min((length - rsz), buflen);
 		res = devs_read(major, minor, addr + rsz, buf, chunk, TEST_DEV_TIMEOUT_MS);
 		if (res < 0) {
 			log_error("\nCan't read data %zd\n", res);
@@ -127,6 +131,11 @@ static int cmd_testDevCheck(int major, int minor, addr_t addr, size_t length)
 static int cmd_doTestDev(int major, int minor, addr_t addr, size_t length, u8 erase)
 {
 	ssize_t res;
+	static u8 buf[BUF_SIZE];
+#if (CMD_TESTDEV_READ_BIG_BUF_SIZE != 0)
+	static u8 bufBig[CMD_TESTDEV_READ_BIG_BUF_SIZE] __attribute__((section(".uncached_ddr")));
+#endif
+	log_setEcho(1);
 
 	res = devs_check(major, minor);
 	if (res < 0) {
@@ -141,7 +150,7 @@ static int cmd_doTestDev(int major, int minor, addr_t addr, size_t length, u8 er
 		}
 	}
 
-	res = cmd_testDevWrite(major, minor, addr, length);
+	res = cmd_testDevWrite(major, minor, addr, length, buf, BUF_SIZE);
 	if (res < 0) {
 		if ((erase & ERASE_AFTER) != 0u) {
 			(void)cmd_testDevErase(major, minor, addr, length);
@@ -160,15 +169,27 @@ static int cmd_doTestDev(int major, int minor, addr_t addr, size_t length, u8 er
 		return res;
 	}
 
-	res = cmd_testDevCheck(major, minor, addr, length);
+	res = cmd_testDevCheck(major, minor, addr, length, buf, BUF_SIZE);
 	if (res < 0) {
-		(void)cmd_testDevRead(major, minor, addr, length);
+		(void)cmd_testDevRead(major, minor, addr, length, buf, BUF_SIZE);
 		if ((erase & ERASE_AFTER) != 0u) {
 			(void)cmd_testDevErase(major, minor, addr, length);
 		}
 
 		return res;
 	}
+
+#if (CMD_TESTDEV_READ_BIG_BUF_SIZE != 0)
+	res = cmd_testDevCheck(major, minor, addr, length, bufBig, CMD_TESTDEV_READ_BIG_BUF_SIZE);
+	if (res < 0) {
+		(void)cmd_testDevRead(major, minor, addr, length, bufBig, CMD_TESTDEV_READ_BIG_BUF_SIZE);
+		if ((erase & ERASE_AFTER) != 0u) {
+			(void)cmd_testDevErase(major, minor, addr, length);
+		}
+
+		return res;
+	}
+#endif
 
 	if ((erase & ERASE_AFTER) != 0u) {
 		res = cmd_testDevErase(major, minor, addr, length);
@@ -185,14 +206,14 @@ static int cmd_doTestDev(int major, int minor, addr_t addr, size_t length, u8 er
 
 static int cmd_testDev(int argc, char *argv[])
 {
-	int major, minor, opt, err;
+	int major = -1, minor = -1, opt, err;
 	u8 erase = 0;
 	char *endptr;
-	addr_t start;
-	size_t length;
+	addr_t start = 0;
+	size_t length = -1;
 
 	for (;;) {
-		opt = lib_getopt(argc, argv, "eE");
+		opt = lib_getopt(argc, argv, "eEd:s:l:");
 		if (opt < 0) {
 			break;
 		}
@@ -205,49 +226,55 @@ static int cmd_testDev(int argc, char *argv[])
 				erase |= ERASE_AFTER;
 				break;
 
+			case 'd':
+				major = lib_strtoul(optarg, &endptr, 0);
+				if (*endptr != '.') {
+					log_error("\nInvalid device.\n");
+					cmd_testDevUsage();
+					return CMD_EXIT_FAILURE;
+				}
+				minor = lib_strtoul(endptr + 1, &endptr, 0);
+				if (*endptr != '\0') {
+					log_error("\nInvalid device.\n");
+					cmd_testDevUsage();
+					return CMD_EXIT_FAILURE;
+				}
+				break;
+
+			case 's':
+				start = lib_strtoul(optarg, &endptr, 0);
+				if (*endptr != '\0') {
+					log_error("\nInvalid start.\n");
+					cmd_testDevUsage();
+					return CMD_EXIT_FAILURE;
+				}
+				break;
+
+			case 'l':
+				length = lib_strtoul(optarg, &endptr, 0);
+				if ((*endptr != '\0') || (length == 0u)) {
+					log_error("\nInvalid length.\n");
+					cmd_testDevUsage();
+					return CMD_EXIT_FAILURE;
+				}
+				break;
+
 			default:
 				cmd_testDevUsage();
 				return CMD_EXIT_FAILURE;
 		}
 	}
-
-	if ((optind + 2) > argc) {
-		lib_consolePuts("\nInvalid arguments.\n");
-		cmd_testDevUsage();
+	if (major == -1) {
+		log_error("\nDevice missing.\n");
 		return CMD_EXIT_FAILURE;
 	}
-
-	major = lib_strtoul(argv[optind], &endptr, 0);
-	if (*endptr != '.') {
-		lib_consolePuts("\nInvalid device.\n");
-		cmd_testDevUsage();
+	if (length == (size_t)-1) {
+		log_error("\nLength missing.\n");
 		return CMD_EXIT_FAILURE;
 	}
-	minor = lib_strtoul(endptr + 1, &endptr, 0);
-	if (*endptr != '\0') {
-		lib_consolePuts("\nInvalid device.\n");
-		cmd_testDevUsage();
-		return CMD_EXIT_FAILURE;
-	}
-	optind++;
-
-	start = lib_strtoul(argv[optind], &endptr, 0);
-	if (*endptr != '\0') {
-		lib_consolePuts("\nInvalid start.\n");
-		cmd_testDevUsage();
-		return CMD_EXIT_FAILURE;
-	}
-	optind++;
-
-	length = lib_strtoul(argv[optind], &endptr, 0);
-	if ((*endptr != '\0') || (length == 0u)) {
-		lib_consolePuts("\nInvalid length.\n");
-		cmd_testDevUsage();
-		return CMD_EXIT_FAILURE;
-	}
-
 	if ((start + length) < start) {
-		length = (addr_t)(-1) - start;
+		log_error("\nStart + length causes overflow.\n");
+		return CMD_EXIT_FAILURE;
 	}
 
 	err = cmd_doTestDev(major, minor, start, length, erase);
