9bb377f044bd53a5880526574146c99311f6298e
lukileczo <lukasz.leczkowski@phoenix-rtos.com>
2023-04-26 12:05:25 +0200

    _startc: copy .rodata section if needed
    JIRA: RTOS-249

--- _startc.c
+++ _startc.c
@@ -15,6 +15,7 @@
 
 extern char __bss_start[], __bss_end[];
 extern char __data_load[], __data_start[], __data_end[];
+extern char __rodata_load[], __rodata_start[], __rodata_end[];
 extern char __ramtext_load[], __ramtext_start[], __ramtext_end[];
 
 extern void (*__init_array_start[])(void);
@@ -31,12 +32,15 @@ void _startc(int argc, char **argv, char **env)
 {
 	size_t i, size;
 
-	/* Load .fastram.text and .data sections */
+	/* Load .fastram.text, .data and .rodata sections */
 	if (__ramtext_start != __ramtext_load)
 		hal_memcpy(__ramtext_start, __ramtext_load, __ramtext_end - __ramtext_start);
 
 	if (__data_start != __data_load)
 		hal_memcpy(__data_start, __data_load, __data_end - __data_start);
+
+	if (__rodata_start != __rodata_load)
+		hal_memcpy(__rodata_start, __rodata_load, __rodata_end - __rodata_start);
 
 	/* Clear the .bss section */
 	hal_memset(__bss_start, 0, __bss_end - __bss_start);
