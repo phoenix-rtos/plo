a5ceba14444f7c7445eda8c9290d38e45cec83df
Łukasz Leczkowski <lukasz.leczkowski@phoenix-rtos.com>
2024-02-29 11:03:15 +0100

    test

--- plo.c
+++ plo.c
@@ -20,8 +20,19 @@
 #include <devices/devs.h>
 #include <syspage.h>
 
+volatile char buf[64] __attribute__((aligned(4)));
+
 static void nop(void)
 {
+#if defined(__ARM_ARCH_7A__)
+	volatile char *x = &buf[1];
+	volatile char *y = &buf[7];
+	__asm__ volatile(
+		"ldr r0, [%0]\n\t"
+		"str r0, [%1]\n\t"
+		: : "r"(x), "r"(y) : "r0");
+#endif
+
 }
 
 void hal_customInit(void) __attribute__((weak, alias("nop")));
