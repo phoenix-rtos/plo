0df38a844e3299c0d6bb380c76a0a0698613faa5
Gerard Swiderski <gerard@swiderski.eu>
2024-03-19 00:01:59 +0100

    ia32/pci: extend PCI bus function set
    The change extends the functionality of PCI bus API with
    features that enable and facilitate the implementation of
    the PCI device drivers.
    
    JIRA: RTOS-595

--- cmds/lspci.c
+++ cmds/lspci.c
@@ -85,47 +85,47 @@ static const char *pciClassName(u8 classId)
 }
 
 
-static void readPCIDevice(u8 bus, u8 dev, u8 func, struct PCIDevice *pciDevice)
+static void readPCIDevice(hal_pciAddr_t *bdf, struct PCIDevice *pciDevice)
 {
-	u32 cfg = hal_pciRead32(bus, dev, func, 0);
+	u32 cfg = hal_pciRead32(bdf, HAL_PCI_REG_PCI_ID / sizeof(u32));
 	pciDevice->vendorID = (u16)cfg;
-	pciDevice->deviceID = (u16)(cfg >> 16);
+	pciDevice->deviceID = (u16)(cfg >> 16u);
 
-	cfg = hal_pciRead32(bus, dev, func, 2);
-	pciDevice->subclass = (u8)(cfg >> 16);
-	pciDevice->classCode = (u8)(cfg >> 24);
+	cfg = hal_pciRead32(bdf, HAL_PCI_REG_CLASS / sizeof(u32));
+	pciDevice->subclass = (u8)(cfg >> 16u);
+	pciDevice->classCode = (u8)(cfg >> 24u);
 
-	cfg = hal_pciRead32(bus, dev, func, 3);
-	pciDevice->headerType = (u8)(cfg >> 16);
+	cfg = hal_pciRead32(bdf, HAL_PCI_REG_HEADER_TYPE / sizeof(u32));
+	pciDevice->headerType = (u8)(cfg >> 16u);
 }
 
 
 static void listDevices(void)
 {
 	struct PCIDevice pciDevice;
-	unsigned int bus;
-	unsigned int dev;
-	unsigned int func;
+	hal_pciAddr_t bdf;
 	const char *name;
 
 	lib_printf("\n\033[1mBUS\tDEVICE\tFUNC\tVID\tDID\tCLASS\tDESCRIPTION\033[0m\n");
 
-	for (bus = 0; bus < 256u; ++bus) {
-		for (dev = 0; dev < 32u; ++dev) {
-			for (func = 0; func < 8u; ++func) {
-				readPCIDevice(bus, dev, func, &pciDevice);
+	for (bdf.bus = 0u; bdf.bus < HAL_PCI_NUM_DEVICES; ++bdf.bus) {
+		for (bdf.device = 0u; bdf.device < HAL_PCI_NUM_DEVICES; ++bdf.device) {
+			for (bdf.function = 0u; bdf.function < HAL_PCI_NUM_FUNCS; ++bdf.function) {
+				readPCIDevice(&bdf, &pciDevice);
 
-				if (pciDevice.vendorID == 0xffff) {
+				if (pciDevice.vendorID == 0xffffu) {
 					continue;
 				}
 
 				lib_printf("0x%02x\t0x%02x\t0x%x\t0x%04x\t0x%04x\t0x%02x%02x\t",
-					bus, dev, func, pciDevice.vendorID, pciDevice.deviceID, pciDevice.classCode, pciDevice.subclass);
+					bdf.bus, bdf.device, bdf.function,
+					pciDevice.vendorID, pciDevice.deviceID,
+					pciDevice.classCode, pciDevice.subclass);
 
 				name = pciClassName(pciDevice.classCode);
 				lib_printf("%.*s\n", 30, (name != NULL) ? name : "Unknown device class");
 
-				if ((func == 0) && ((pciDevice.headerType & 0x80) == 0)) {
+				if ((bdf.function == 0u) && ((pciDevice.headerType & 0x80u) == 0u)) {
 					break;
 				}
 			}
@@ -134,7 +134,7 @@ static void listDevices(void)
 }
 
 
-static void dumpConfig(u8 bus, u8 dev, u8 func)
+static void dumpConfig(hal_pciAddr_t *bdf)
 {
 	size_t offs;
 	u32 val;
@@ -146,13 +146,13 @@ static void dumpConfig(u8 bus, u8 dev, u8 func)
 			lib_printf("0x%02x   | ", offs * sizeof(val));
 		}
 
-		val = hal_pciRead32(bus, dev, func, offs);
+		val = hal_pciRead32(bdf, offs);
 		lib_printf("0x%08x%c", val, ((offs & 3) == 3) ? '\n' : ' ');
 	}
 }
 
 
-static int parseBDF(char *str, u8 *bus, u8 *dev, u8 *func)
+static int parseBDF(char *str, hal_pciAddr_t *bdf)
 {
 	unsigned int val;
 	char *ptr = str;
@@ -165,21 +165,21 @@ static int parseBDF(char *str, u8 *bus, u8 *dev, u8 *func)
 	if ((*ptr != ':') || (val > 0xff)) {
 		return -EINVAL;
 	}
-	*bus = val;
+	bdf->bus = val;
 
 	ptr++;
 	val = lib_strtoul(ptr, &ptr, 0);
 	if ((*ptr != ':') || (val > 0x1f)) {
 		return -EINVAL;
 	}
-	*dev = val;
+	bdf->device = val;
 
 	ptr++;
 	val = lib_strtoul(ptr, &ptr, 0);
 	if ((*ptr != '\0') || (val > 7)) {
 		return -EINVAL;
 	}
-	*func = val;
+	bdf->function = val;
 
 	return 0;
 }
@@ -189,7 +189,7 @@ static int cmd_lspciMain(int argc, char *argv[])
 {
 	int opt;
 	int optDumpRegs = 0;
-	u8 bus, dev, func;
+	hal_pciAddr_t bdf = { 0 };
 
 	lib_printf("\n");
 
@@ -200,7 +200,7 @@ static int cmd_lspciMain(int argc, char *argv[])
 		}
 
 		if (opt == 'd') {
-			if ((optDumpRegs == 0) && (parseBDF(optarg, &bus, &dev, &func) == 0)) {
+			if ((optDumpRegs == 0) && (parseBDF(optarg, &bdf) == 0)) {
 				optDumpRegs = 1;
 				continue;
 			}
@@ -217,7 +217,7 @@ static int cmd_lspciMain(int argc, char *argv[])
 	}
 
 	if (optDumpRegs != 0) {
-		dumpConfig(bus, dev, func);
+		dumpConfig(&bdf);
 	}
 	else {
 		listDevices();
