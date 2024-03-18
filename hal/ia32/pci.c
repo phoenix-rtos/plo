901fdc2a7b932353be719bb23c3e43dee411641e
Gerard Swiderski <gerard@swiderski.eu>
2024-03-19 00:01:59 +0100

    ia32/pci: extend PCI bus function set
    The change extends the functionality of PCI bus API with
    features that enable and facilitate the implementation of
    the PCI device drivers.
    
    JIRA: RTOS-595

--- hal/ia32/pci.c
+++ hal/ia32/pci.c
@@ -5,7 +5,7 @@
  *
  * PCI bus access
  *
- * Copyright 2023 Phoenix Systems
+ * Copyright 2023-2024 Phoenix Systems
  * Author: Gerard Swiderski
  *
  * This file is part of Phoenix-RTOS.
@@ -16,6 +16,7 @@
 
 #include <hal/hal.h>
 #include "types.h"
+#include "pci.h"
 
 
 #define PCI_CONFIG_ADDR 0xcf8u
@@ -30,36 +31,132 @@ int hal_pciDetect(void)
 }
 
 
-u32 hal_pciAddrBDF(u8 bus, u8 dev, u8 func)
+u32 hal_pciAddrBDF(hal_pciAddr_t *bdf)
 {
 	return PCI_ENABLE |
-		(((u32)bus & 0xffu) << 16) |
-		(((u32)dev & 0x1fu) << 11) |
-		(((u32)func & 7u) << 8);
+		(((u32)bdf->bus & 0xffu) << 16) |
+		(((u32)bdf->device & 0x1fu) << 11) |
+		(((u32)bdf->function & 7u) << 8);
 }
 
 
-u32 hal_pciRead32(u8 bus, u8 dev, u8 func, u8 offs)
+u32 hal_pciRead32(hal_pciAddr_t *bdf, u8 offs)
 {
-	u32 bdf = hal_pciAddrBDF(bus, dev, func);
+	u32 addr = hal_pciAddrBDF(bdf);
 	offs &= 256u / sizeof(u32) - 1u;
-	hal_outl((void *)PCI_CONFIG_ADDR, bdf + ((u32)offs << 2u));
+	hal_outl((void *)PCI_CONFIG_ADDR, addr + ((u32)offs << 2u));
 	return hal_inl((void *)PCI_CONFIG_DATA);
 }
 
 
-u16 hal_pciRead16(u8 bus, u8 dev, u8 func, u8 offs)
+void hal_pciWrite32(hal_pciAddr_t *bdf, u8 offs, u32 value)
 {
-	u32 bdf = hal_pciAddrBDF(bus, dev, func);
+	u32 addr = hal_pciAddrBDF(bdf);
+	offs &= 256u / sizeof(u32) - 1u;
+	hal_outl((void *)PCI_CONFIG_ADDR, addr + ((u32)offs << 2u));
+	hal_outl((void *)PCI_CONFIG_DATA, value);
+}
+
+
+u16 hal_pciRead16(hal_pciAddr_t *bdf, u8 offs)
+{
+	u32 addr = hal_pciAddrBDF(bdf);
 	offs &= 256u / sizeof(u16) - 1u;
-	hal_outl((void *)PCI_CONFIG_ADDR, bdf + ((u32)offs << 1u));
+	hal_outl((void *)PCI_CONFIG_ADDR, addr + ((u32)offs << 1u));
 	return hal_inw((void *)(PCI_CONFIG_DATA + (((addr_t)offs & 1u) << 1u)));
 }
 
 
-u8 hal_pciRead8(u8 bus, u8 dev, u8 func, u8 offs)
+void hal_pciWrite16(hal_pciAddr_t *bdf, u8 offs, u16 value)
 {
-	u32 bdf = hal_pciAddrBDF(bus, dev, func);
-	hal_outl((void *)PCI_CONFIG_ADDR, bdf + (u32)offs);
+	u32 addr = hal_pciAddrBDF(bdf);
+	offs &= 256u / sizeof(u16) - 1u;
+	hal_outl((void *)PCI_CONFIG_ADDR, addr + ((u32)offs << 1u));
+	hal_outw((void *)(PCI_CONFIG_DATA + (((addr_t)offs & 1u) << 1u)), value);
+}
+
+
+u8 hal_pciRead8(hal_pciAddr_t *bdf, u8 offs)
+{
+	u32 addr = hal_pciAddrBDF(bdf);
+	hal_outl((void *)PCI_CONFIG_ADDR, addr + (u32)offs);
 	return hal_inb((void *)(PCI_CONFIG_DATA + ((addr_t)offs & 3u)));
+}
+
+
+void hal_pciWrite8(hal_pciAddr_t *bdf, u8 offs, u8 value)
+{
+	u32 addr = hal_pciAddrBDF(bdf);
+	hal_outl((void *)PCI_CONFIG_ADDR, addr + (u32)offs);
+	hal_outb((void *)(PCI_CONFIG_DATA + ((addr_t)offs & 3u)), value);
+}
+
+
+void hal_pciBusMaster(hal_pciAddr_t *bdf, int enableBM, int enableIO, int enableMem)
+{
+	u16 command = hal_pciRead16(bdf, HAL_PCI_REG_COMMAND / sizeof(u16));
+
+	/* Enable/Disable bus mastering */
+	if (enableBM > 0) {
+		command |= HAL_PCI_COMMAND_BUS_MASTER;
+	}
+	if (enableBM == 0) {
+		command &= ~HAL_PCI_COMMAND_BUS_MASTER;
+	}
+
+	/* Enable/Disable address spacess */
+	if (enableIO > 0) {
+		command |= HAL_PCI_COMMAND_IO_ENABLED;
+	}
+	if (enableIO == 0) {
+		command &= ~HAL_PCI_COMMAND_IO_ENABLED;
+	}
+	if (enableMem > 0) {
+		command |= HAL_PCI_COMMAND_MEM_ENABLED;
+	}
+	if (enableMem == 0) {
+		command &= ~HAL_PCI_COMMAND_MEM_ENABLED;
+	}
+
+	hal_pciWrite16(bdf, HAL_PCI_REG_COMMAND / sizeof(u16), command);
+}
+
+
+void hal_pciIterate(int (*cb)(hal_pciAddr_t bdf, u32 id, void *data), void *cbData)
+{
+	hal_pciAddr_t bdf;
+
+	for (bdf.bus = 0u; bdf.bus < HAL_PCI_NUM_BUS; ++bdf.bus) {
+		for (bdf.device = 0u; bdf.device < HAL_PCI_NUM_DEVICES; ++bdf.device) {
+			for (bdf.function = 0u; bdf.function < HAL_PCI_NUM_FUNCS; ++bdf.function) {
+				u32 id = hal_pciRead32(&bdf, HAL_PCI_REG_PCI_ID / sizeof(u32));
+
+				/* Check if there is a device present */
+				if (((id >> 16u) & 0xffffu) == 0xffffu) {
+					if (bdf.function == 0u) {
+						/*
+						 * Devices are required to implement func=0,
+						 * so if it is missing then there is not present
+						 */
+						break;
+					}
+					else {
+						continue;
+					}
+				}
+
+				if (cb(bdf, id, cbData) != 0) {
+					return;
+				}
+
+				/* Probe only func=0 if the device if not multifunction */
+				if (bdf.function == 0u) {
+					u32 hdr = hal_pciRead32(&bdf, HAL_PCI_REG_CACHELINE / sizeof(u32));
+					if ((hdr & (1uL << 23u)) != 0uL) {
+						break;
+					}
+				}
+			}
+		}
+	}
 }
--- hal/ia32/pci.h
+++ hal/ia32/pci.h
@@ -5,7 +5,7 @@
  *
  * PCI bus access
  *
- * Copyright 2023 Phoenix Systems
+ * Copyright 2023-2024 Phoenix Systems
  * Author: Gerard Swiderski
  *
  * This file is part of Phoenix-RTOS.
@@ -21,24 +21,77 @@
 #include <types.h>
 
 
+typedef struct {
+	unsigned int bus;
+	unsigned int device;
+	unsigned int function;
+} hal_pciAddr_t;
+
+
+#define HAL_PCI_NUM_BUS     256u
+#define HAL_PCI_NUM_DEVICES 32u
+#define HAL_PCI_NUM_FUNCS   8u
+
+
+#define HAL_PCI_REG_PCI_ID      0x00u
+#define HAL_PCI_REG_VENDOR      0x00u
+#define HAL_PCI_REG_DEVICE      0x02u
+#define HAL_PCI_REG_COMMAND     0x04u
+#define HAL_PCI_REG_STATUS      0x06u
+#define HAL_PCI_REG_REVISION    0x08u
+#define HAL_PCI_REG_CLASS       0x08u
+#define HAL_PCI_REG_CACHELINE   0x0cu
+#define HAL_PCI_REG_LAT_TIMER   0x0du
+#define HAL_PCI_REG_HEADER_TYPE 0x0eu
+#define HAL_PCI_REG_BIST        0x0fu
+#define HAL_PCI_REG_ADDRESSES   0x10u
+
+
+#define HAL_PCI_COMMAND_IO_ENABLED   (1uL << 0u)
+#define HAL_PCI_COMMAND_MEM_ENABLED  (1uL << 1u)
+#define HAL_PCI_COMMAND_BUS_MASTER   (1uL << 2u)
+#define HAL_PCI_COMMAND_PARITY_ERROR (1uL << 6u)
+#define HAL_PCI_COMMAND_SERR_ENABLE  (1uL << 8u)
+
+
 /* Function checks presence of PCI bus */
 extern int hal_pciDetect(void);
 
 
 /* Function makes PCI address */
-extern u32 hal_pciAddrBDF(u8 bus, u8 dev, u8 func);
+extern u32 hal_pciAddrBDF(hal_pciAddr_t *bdf);
 
 
 /* Function reads 32bit word of PCI configuration */
-extern u32 hal_pciRead32(u8 bus, u8 dev, u8 func, u8 offs);
+extern u32 hal_pciRead32(hal_pciAddr_t *bdf, u8 offs);
 
 
 /* Function reads 16bit half word of PCI configuration */
-extern u16 hal_pciRead16(u8 bus, u8 dev, u8 func, u8 offs);
+extern u16 hal_pciRead16(hal_pciAddr_t *bdf, u8 offs);
 
 
 /* Function reads 8bit byte of PCI configuration */
-extern u8 hal_pciRead8(u8 bus, u8 dev, u8 func, u8 offs);
+extern u8 hal_pciRead8(hal_pciAddr_t *bdf, u8 offs);
+
+
+/* Function writes 32bit word of PCI configuration */
+void hal_pciWrite32(hal_pciAddr_t *bdf, u8 offs, u32 value);
+
+
+/* Function writes 16bit half word of PCI configuration */
+void hal_pciWrite16(hal_pciAddr_t *bdf, u8 offs, u16 value);
+
+
+/* Function writes 8bit byte of PCI configuration */
+void hal_pciWrite8(hal_pciAddr_t *bdf, u8 offs, u8 value);
+
+
+/* Commands PCI device to generate accesses: bus mastering and mem/io address spaces */
+void hal_pciBusMaster(hal_pciAddr_t *bdf, int enableBM, int enableIO, int enableMem);
+
+
+/* Helper function to simplify pci device detection */
+void hal_pciIterate(int (*cb)(hal_pciAddr_t bdf, u32 id, void *data), void *cbData);
 
 
 #endif /* end of _PCI_H_ */
