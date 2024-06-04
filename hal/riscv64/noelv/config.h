8700c191341151e7d5636088b844fcdd4100941a
Łukasz Leczkowski <lukasz.leczkowski@phoenix-rtos.com>
2024-06-04 14:48:07 +0200

    riscv64: add noelv target
    JIRA: RTOS-844

---
+++ hal/riscv64/noelv/config.h
@@ -0,0 +1,42 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Operating system loader
+ *
+ * Platform configuration
+ *
+ * Copyright 2024 Phoenix Systems
+ * Author: Lukasz Leczkowski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+
+#ifndef __ASSEMBLY__
+
+#include "peripherals.h"
+#include "types.h"
+#include "hal/riscv64/cpu.h"
+#include "hal/riscv64/plic.h"
+#include "hal/riscv64/sbi.h"
+#include "hal/riscv64/dtb.h"
+
+#include <phoenix/arch/riscv64/syspage.h>
+#include <phoenix/syspage.h>
+
+#define PATH_KERNEL "phoenix-riscv64-noelv.elf"
+
+#endif /* __ASSEMBLY__ */
+
+#include "noelv.h"
+
+/* Import platform specific definitions */
+#include "ld/riscv64-noelv.ldt"
+
+
+#endif
