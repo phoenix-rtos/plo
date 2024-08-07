dcc2fa2564d9249828dcee2363df8efe73f903f0
Łukasz Leczkowski <lukasz.leczkowski@phoenix-rtos.com>
2024-08-07 13:23:52 +0200

    hal/sparcv8leon3: refactor timer register definitions
    JIRA: RTOS-885

--- hal/sparcv8leon3/gaisler/gr712rc/peripherals.h
+++ hal/sparcv8leon3/gaisler/gr712rc/peripherals.h
@@ -18,28 +18,6 @@
 
 #include <board_config.h>
 
-/* Timer registers */
-
-#define GPT_SCALER  0 /* Scaler value register        : 0x00 */
-#define GPT_SRELOAD 1 /* Scaler reload value register : 0x04 */
-#define GPT_CONFIG  2 /* Configuration register       : 0x08 */
-
-#define GPT_TCNTVAL1 4 /* Timer 1 counter value reg    : 0x10 */
-#define GPT_TRLDVAL1 5 /* Timer 1 reload value reg     : 0x14 */
-#define GPT_TCTRL1   6 /* Timer 1 control register     : 0x18 */
-
-#define GPT_TCNTVAL2 8  /* Timer 2 counter value reg    : 0x20 */
-#define GPT_TRLDVAL2 9  /* Timer 2 reload value reg     : 0x24 */
-#define GPT_TCTRL2   10 /* Timer 2 control register     : 0x28 */
-
-#define GPT_TCNTVAL3 12 /* Timer 3 counter value reg    : 0x30 */
-#define GPT_TRLDVAL3 13 /* Timer 3 reload value reg     : 0x34 */
-#define GPT_TCTRL3   14 /* Timer 3 control register     : 0x38 */
-
-#define GPT_TCNTVAL4 16 /* Timer 4 counter value reg    : 0x40 */
-#define GPT_TRLDVAL4 17 /* Timer 4 reload value reg     : 0x44 */
-#define GPT_TCTRL4   18 /* Timer 4 control register     : 0x48 */
-
 /* Arbitrarily chosen address */
 #define RAM_ADDR      (0x47e00000)
 #define RAM_BANK_SIZE (0x200000)
