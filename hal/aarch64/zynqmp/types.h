40fc6ff2a1cff90feef5ccac17446def5d6d5d4f
Jacek Maksymowicz <jacek.maksymowicz@phoenix-rtos.com>
2024-11-12 11:07:57 +0100

    hal: add support for AArch64 architecture
    Add support for ZynqMP platform.
    
    JIRA: RTOS-949

---
+++ hal/aarch64/zynqmp/types.h
@@ -0,0 +1,46 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Operating system loader
+ *
+ * Types
+ *
+ * Copyright 2021 Phoenix Systems
+ * Author: Hubert Buczynski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+#define NULL ((void *)0)
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+typedef signed char s8;
+typedef short s16;
+typedef int s32;
+typedef long long s64;
+
+typedef volatile unsigned char vu8;
+typedef volatile unsigned short vu16;
+typedef volatile unsigned int vu32;
+typedef volatile unsigned long long vu64;
+
+typedef vu8 *reg8;
+typedef vu16 *reg16;
+typedef vu32 *reg32;
+typedef vu64 *reg64;
+
+typedef u64 addr_t;
+typedef u64 size_t;
+typedef s64 ssize_t;
+typedef unsigned long long time_t;
+
+#endif
