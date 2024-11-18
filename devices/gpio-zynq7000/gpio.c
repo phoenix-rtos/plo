36068aef853d17a0d4030150a916e0147481d591
Jacek Maksymowicz <jacek.maksymowicz@phoenix-rtos.com>
2024-11-18 16:21:48 +0100

    zynq7000: add support for peripherals on zynqmp platform
    Applies to gpio-zynq7000, uart-zynq7000, flash-zynq7000.
    These peripherals are similar enough that they can be supported
    by the same code with only some constants and definition changes.

--- devices/gpio-zynq7000/gpio.c
+++ devices/gpio-zynq7000/gpio.c
@@ -18,28 +18,48 @@
 #include <lib/errno.h>
 
 
-#define MAX_BANK_NB    4
 #define BANK_REGS_OFFS 0x10
 
-
+/* clang-format off */
 enum {
-	data_lsw = 0, data_msw, mask_data1_lsw, mask_data1_msw, mask_data2_lsw, mask_data2_msw, mask_data3_lsw, mask_data3_msw,
-	data = 0x10, data1, data2, data3,
-	data_ro = 0x18, data1_ro, data2_ro, data3_ro,
-	dirm = 0x81, oen, int_mask, int_en, int_dis, int_stat, int_type, int_pol, int_any,
-	dirm1 = 0x91, oen1, int_mask1, int_en1, int_dis1, int_stat1, int_type1, int_pol1, int_any1,
-	dirm2 = 0xa1, oen2, int_mask2, int_en2, int_dis2, int_stat2, int_type2, int_pol2, int_any2,
-	dirm3 = 0xb1, oen3, int_mask3, int_en3, int_dis3, int_stat3, int_type3, int_pol3, int_any3,
+	data_lsw = 0, data_msw, /* up to MAX_BANK_NB pairs of 2 registers*/
+	data = 0x10, /* up to MAX_BANK_NB registers */
+	data_ro = 0x18, /* up to MAX_BANK_NB registers */
+	dirm = 0x81, oen, int_mask, int_en, int_dis, int_stat, int_type, int_pol, int_any, /* MAX_BANK_NB banks, BANK_REGS_OFFS stride */
 };
+/* clang-format on */
 
+#if defined(__CPU_ZYNQ7000)
+#define MAX_BANK_NB 4
 
 const struct {
 	u32 min;
 	u32 max;
-} pinsPos[MAX_BANK_NB] = { { 0, 31 },     /* Bank0: MIO pins[0:31]      */
-                           { 32, 53 },    /* Bank1: MIO pins[32:53]     */
-                           { 54, 85 },    /* Bank2: EMIO signals[0:31]  */
-                           { 86, 117 } }; /* Bank3: EMIO signals[32:63] */
+} pinsPos[MAX_BANK_NB] = {
+	{ 0, 31 },   /* Bank0: MIO pins[0:31]      */
+	{ 32, 53 },  /* Bank1: MIO pins[32:53]     */
+	{ 54, 85 },  /* Bank2: EMIO signals[0:31]  */
+	{ 86, 117 }, /* Bank3: EMIO signals[32:63] */
+};
+
+#elif defined(__CPU_ZYNQMP)
+#define MAX_BANK_NB 6
+
+const struct {
+	u32 min;
+	u32 max;
+} pinsPos[MAX_BANK_NB] = {
+	{ 0, 25 },            /* Bank0: MIO pins[0:25]      */
+	{ 26, 51 },           /* Bank1: MIO pins[26:51]     */
+	{ 52, 77 },           /* Bank2: MIO pins[52:77]     */
+	{ 78 + 0, 78 + 31 },  /* Bank3: EMIO signals[0:31]  */
+	{ 78 + 32, 78 + 63 }, /* Bank4: EMIO signals[32:63]  */
+	{ 78 + 64, 78 + 95 }, /* Bank5: EMIO signals[64:95]  */
+};
+
+#else
+#error "Unknown target platform"
+#endif
 
 
 struct {
@@ -51,8 +71,9 @@ static int gpio_getPinPos(u8 pin, u8 *bank, u8 *pos)
 {
 	int i;
 
-	if (pin > pinsPos[MAX_BANK_NB - 1].max)
+	if (pin > pinsPos[MAX_BANK_NB - 1].max) {
 		return -EINVAL;
+	}
 
 	for (i = 0; i < MAX_BANK_NB; ++i) {
 		if (pin >= pinsPos[i].min && pin <= pinsPos[i].max) {
@@ -72,8 +93,9 @@ int gpio_writePin(u8 pin, u8 val)
 	u32 v;
 	u8 bank, pos;
 
-	if (gpio_getPinPos(pin, &bank, &pos) < 0)
+	if (gpio_getPinPos(pin, &bank, &pos) < 0) {
 		return -EINVAL;
+	}
 
 	v = *(gpio_common.base + data + (BANK_REGS_OFFS * bank));
 	*(gpio_common.base + data + (BANK_REGS_OFFS * bank)) = (v & ~(1 << pos)) | (!!val << pos);
@@ -87,8 +109,9 @@ int gpio_readPin(u8 pin, u8 *val)
 	u32 v;
 	u8 bank, pos;
 
-	if (gpio_getPinPos(pin, &bank, &pos) < 0)
+	if (gpio_getPinPos(pin, &bank, &pos) < 0) {
 		return -EINVAL;
+	}
 
 	v = *(gpio_common.base + data_ro + (BANK_REGS_OFFS * bank));
 	*val = !!(v & (1 << pos));
@@ -102,8 +125,9 @@ int gpio_getPinDir(u8 pin, u8 *dir)
 	u32 d;
 	u8 bank, pos;
 
-	if (gpio_getPinPos(pin, &bank, &pos) < 0)
+	if (gpio_getPinPos(pin, &bank, &pos) < 0) {
 		return -EINVAL;
+	}
 
 	d = *(gpio_common.base + dirm + (BANK_REGS_OFFS * bank));
 	*dir = !!(d & (1 << pos));
@@ -117,8 +141,9 @@ int gpio_setPinDir(u8 pin, u8 dir)
 	u32 d, v, offs;
 	u8 bank, pos;
 
-	if (gpio_getPinPos(pin, &bank, &pos) < 0)
+	if (gpio_getPinPos(pin, &bank, &pos) < 0) {
 		return -EINVAL;
+	}
 
 	offs = BANK_REGS_OFFS * bank;
 	d = *(gpio_common.base + dirm + offs);
@@ -133,8 +158,9 @@ int gpio_setPinDir(u8 pin, u8 dir)
 
 int gpio_writeBank(u8 bank, u32 val)
 {
-	if (bank >= MAX_BANK_NB)
+	if (bank >= MAX_BANK_NB) {
 		return -EINVAL;
+	}
 
 	*(gpio_common.base + data + (BANK_REGS_OFFS * bank)) = val;
 
@@ -144,8 +170,9 @@ int gpio_writeBank(u8 bank, u32 val)
 
 int gpio_readBank(u8 bank, u32 *val)
 {
-	if (bank >= MAX_BANK_NB)
+	if (bank >= MAX_BANK_NB) {
 		return -EINVAL;
+	}
 
 	*val = *(gpio_common.base + data_ro + (BANK_REGS_OFFS * bank));
 
@@ -155,8 +182,9 @@ int gpio_readBank(u8 bank, u32 *val)
 
 int gpio_getBankDir(u8 bank, u32 *dir)
 {
-	if (bank >= MAX_BANK_NB)
+	if (bank >= MAX_BANK_NB) {
 		return -EINVAL;
+	}
 
 	*dir = *(gpio_common.base + dirm + (BANK_REGS_OFFS * bank));
 
@@ -166,8 +194,9 @@ int gpio_getBankDir(u8 bank, u32 *dir)
 
 int gpio_setBankDir(u8 bank, u32 dir)
 {
-	if (bank >= MAX_BANK_NB)
+	if (bank >= MAX_BANK_NB) {
 		return -EINVAL;
+	}
 
 	*(gpio_common.base + dirm + (BANK_REGS_OFFS * bank)) = dir;
 	*(gpio_common.base + oen + (BANK_REGS_OFFS * bank)) = dir;
@@ -180,11 +209,19 @@ void gpio_init(void)
 {
 	gpio_common.base = GPIO_BASE_ADDR;
 
+#if defined(__CPU_ZYNQ7000)
 	_zynq_setAmbaClk(amba_gpio_clk, clk_enable);
+#elif defined(__CPU_ZYNQMP)
+	_zynqmp_devReset(ctl_reset_lpd_gpio, 0);
+#endif
 }
 
 
 void gpio_deinit(void)
 {
+#if defined(__CPU_ZYNQ7000)
 	_zynq_setAmbaClk(amba_gpio_clk, clk_disable);
+#elif defined(__CPU_ZYNQMP)
+	_zynqmp_devReset(ctl_reset_lpd_gpio, 1);
+#endif
 }
