f4f5459f97b07af814f0b6e87bff4de624ba8085
winderdoot <krzysztof.radzewicz@phoenix-rtos.com>
2025-11-06 16:30:00 +0100

    hal/stm32n6: add otp controller
    Enable otp command for STM32N6 in Makefile.
    
    JIRA: RTOS-1068

---
+++ hal/armv8m/stm32/n6/otp.h
@@ -0,0 +1,34 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Operating system loader
+ *
+ * BSEC OTP control STM32N6 series
+ *
+ * Copyright 2020-2022 2025 Phoenix Systems
+ * Author: Aleksander Kaminski, Gerard Swiderski, Krzysztof Radzewicz
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#include <../types.h>
+
+#ifndef _OTP_H_
+#define _OTP_H_
+
+
+int otp_checkFuseValid(int fuse);
+
+
+int otp_read(int fuse, u32 *val);
+
+
+int otp_write(int fuse, u32 val);
+
+
+void otp_init(void);
+
+
+#endif /* _OTP_H_ */
