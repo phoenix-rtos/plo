65d68201d58394e6790fc3f7cc0f7ecb9eaf5c18
Jacek Maksymowicz <jacek.maksymowicz@phoenix-rtos.com>
2025-12-11 11:50:16 +0100

    devices/flash-stm32xspi: restore previous prefetch settings
    After more extensive testing it was determined that enabling prefetch on
    all transactions was still causing data corruption.
    
    JIRA: RTOS-1182

--- devices/flash-stm32xspi/xspi_common.c
+++ devices/flash-stm32xspi/xspi_common.c
@@ -462,10 +462,11 @@ static int xspidrv_init(unsigned int minor)
 
 	/* NOTE: we set the NOPREF_AXI bit, during testing clearing it resulted in data corruption in some situations */
 	*(p->ctrl + xspi_cr) =
+			(1UL << 26) | /* Prefetch is disabled when the AXI transaction is signaled as not-prefetchable */
 			(p->chipSelect << 24) |
-			(1 << 22) | /* Stop auto-polling on match */
-			(3 << 8) |  /* FIFO threshold = 4 bytes */
-			(1 << 3);   /* Enable timeout for memory-mapped mode */
+			(1UL << 22) | /* Stop auto-polling on match */
+			(3UL << 8) |  /* FIFO threshold = 4 bytes */
+			(1UL << 3);   /* Enable timeout for memory-mapped mode */
 
 	*(p->ctrl + xspi_lptr) = 64; /* Timeout for memory-mapped mode */
 	xspi_waitBusy(minor);
